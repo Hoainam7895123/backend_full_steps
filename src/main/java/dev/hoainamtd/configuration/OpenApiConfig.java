08813cbff5540259909b07afc568d1b1af7fbc6a
devhoainamtd <truongdinhhoainam1703@gmail.com>
2024-10-15 09:08:10 +0700

    API document

---
+++ src/main/java/dev/hoainamtd/configuration/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package dev.hoainamtd.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${open.api.title}") String title,
+                           @Value("${open.api.version}") String version,
+                           @Value("${open.api.description}") String description,
+                           @Value("${open.api.serverUrl}") String serverUrl,
+                           @Value("${open.api.serverName}") String serverName
+                           ) {
+        return new OpenAPI().info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                        .license(new License().name("API License").url("http://domain.vn/license")))
+                .servers(List.of(new Server().url(serverUrl).description(serverName)));
+
+    }
+
+    //Tich hop cai url vd /v3/api-docs de tich vao micro service
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("api-service-1")
+                .packagesToScan("dev.hoainamtd.controller")
+                .build();
+    }
+}
--- src/main/java/dev/hoainamtd/controller/UserController.java
+++ src/main/java/dev/hoainamtd/controller/UserController.java
@@ -3,9 +3,10 @@ package dev.hoainamtd.controller;
 import dev.hoainamtd.configuration.Translator;
 import dev.hoainamtd.dto.response.ResponseData;
 import dev.hoainamtd.dto.response.ResponseError;
-import dev.hoainamtd.dto.response.ResponseSuccess;
 import dev.hoainamtd.dto.reuqest.UserRequestDTO;
 import dev.hoainamtd.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @Validated
+@Tag(name = "User Controller")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Add user", description = "API create new user")
     @PostMapping("/")
     public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO user) {
         try {
@@ -35,30 +38,35 @@ public class UserController {
 
     }
 
+    @Operation(summary = "Update user", description = "Send a request via this API to update user")
     @PutMapping("/{userId}")
     public ResponseData<?> updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
         System.out.println("Request update userId=" + userId);
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated successfully");
     }
 
+    @Operation(summary = "Change status of user", description = "Send a request via this API to change status of user")
     @PatchMapping("/{userId}")
     public ResponseData<?> updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
         System.out.println("Request change status, userId=" + userId);
         return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User's status changed successfully");
     }
 
+    @Operation(summary = "Delete user permanently", description = "Send a request via this API to delete user permanently")
     @DeleteMapping("/{userId}")
     public ResponseData<?> deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
         System.out.println("Request delete userId=" + userId);
         return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User deleted successfully");
     }
 
+    @Operation(summary = "Get user detail", description = "Send a request via this API to get user information")
     @GetMapping("/{userId}")
     public ResponseData<UserRequestDTO> getUser(@PathVariable @Min(1) int userId) {
         System.out.println("Request get user detail, userId=" + userId);
         return new ResponseData<>(HttpStatus.OK.value(), "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", "0123456789"));
     }
 
+    @Operation(summary = "Get list of users per pageNo", description = "Send a request via this API to get user list by pageNo and pageSize")
     @GetMapping("/list")
     public ResponseData<List<UserRequestDTO>> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
                                                          @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
